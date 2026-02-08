ee29779dcf23095680e6a62657328b487670f086
Rich Bell <bellrichm@gmail.com>
2026-02-08 07:43:19 -0500

    linting

--- bin/user/tests/func/test_datagenerator.py
+++ bin/user/tests/func/test_datagenerator.py
@@ -13,7 +13,6 @@ import mock
 from user.tests import helpers
 
 import configobj
-import datetime
 import os
 import time
 
@@ -58,7 +57,7 @@ class TestDataGenerator(unittest.TestCase):
             self.assertEqual(result, result1.format(now=now))
 
             result = generator._gen_data_load(time_span, 'foo', 'day', 'active', 'day', 'day_')
-        #  print(result)
+            #  print(result)
             self.assertEqual(result, result2.format(now=now, utc_offset=utc_offset))
 
             print("done 1")
