481864b356c034a02fc51b458d2261e710a839e7
Rich Bell <bellrichm@gmail.com>
2026-03-26 11:52:36 -0400

    test page layout is not grid

--- bin/user/tests/unit/data/template_results/body_inc.py
+++ bin/user/tests/unit/data/template_results/body_inc.py
@@ -180,3 +180,55 @@ result_page_has_file_alert_modal_inc = \
 
 
 '''
+
+result_page_layout_is_not_grid = \
+'''
+
+
+    <div class="container">
+
+
+<button id="connectButton" class="btn d-none" type="button" text_label="connect_button_label" onclick="MQTTConnect()"></button>
+<button id="disconnectButton" class="btn d-none" type="button" text_label="disconnect_button_label" onclick="MQTTDisconnect()"></button>
+
+<div class="col-sm-10 mb-4">
+  <button type="button" class="btn" onclick="copyLog()">Copy Log</button>
+  <button type="button" class="btn" onclick="clearLog()">Clear Log</button>
+</div>
+
+<div class="col-sm-10 mb-4">
+  <div class="text-wrap" id="logDisplay"
+    style="overflow-y:scroll; height:300px">
+  </div>
+</div>
+
+<script>
+  if (window.self === window.top) {
+    document.getElementById('connectButton').classList.remove("d-none");
+    document.getElementById('disconnectButton').classList.remove("d-none");
+  }
+</script>           </div>
+
+    </div>
+      <script src="../charts/bar2.js"></script>
+    
+    <!-- Modal -->
+<div class="modal" id="chartModal" tabindex="-1" role="dialog" aria-labelledby="chartModalTitle" aria-hidden="true">
+  <div class="modal-dialog modal-xl" role="document">
+    <div class="modal-content">
+      <div class="modal-header" id="chartModalHeader">
+        <h5 class="modal-title" id="chartModalTitle">Modal title</h5>
+        <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body" id="chartModalBody">
+        ...
+      </div>
+      <div class="modal-footer" id="chartModalFooter"></div>
+    </div>
+  </div>
+</div>
+
+
+'''
--- bin/user/tests/unit/test_templates.py
+++ bin/user/tests/unit/test_templates.py
@@ -16,7 +16,7 @@ import types
 from user.tests import helpers
 
 from user.tests.unit.data.template_results.body_inc import result_page_has_no_sections, result_page_has_zoom_control, result_page_has_section_debug,\
-      result_page_has_file_alert_modal_inc
+      result_page_has_file_alert_modal_inc, result_page_layout_is_not_grid
 
 class TestBodyInc(unittest.TestCase):
     @classmethod
@@ -167,6 +167,43 @@ class TestBodyInc(unittest.TestCase):
         template_instance = template_class(searchList=[data])
         result = template_instance.respond()
         self.assertEqual(result, result_page_has_file_alert_modal_inc)
+
+    def test_page_layout_is_not_grid(self):
+        self.maxDiff = None
+
+        page_name = 'foo1'
+        extras = types.SimpleNamespace(
+            pages = {
+                page_name: {
+                    'debug': {
+                        'layout': 'not grid'
+                    }
+                }
+            },
+            chart_definitions = 'bar1',
+            current = {
+                'observations': {
+                    'obs1': {
+                        'display': False
+                    }
+                }
+            },
+        )
+
+        data = {
+            'page': page_name,
+            'Extras': extras,
+            'page_name_global': 'bar2',
+        }
+
+        filename = 'generators/body.inc'
+
+        template_class = Cheetah.Template.Template.compile(file=filename)
+        # print(f"----\n{Cheetah.Template.Template.generatedModuleCode(template_class)}\n----")
+        template_instance = template_class(searchList=[data])
+        result = template_instance.respond()
+        print(f"----\n{result}\n----")
+        self.assertEqual(result, result_page_layout_is_not_grid)
 
 if __name__ == '__main__':
     helpers.run_tests()
