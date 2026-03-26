9479b1c5a18182e9180c924dbd7142916788f04c
Rich Bell <bellrichm@gmail.com>
2026-03-26 06:00:32 -0400

    first test and refactor

---
+++ bin/user/tests/unit/data/template_results/body_inc.py
@@ -0,0 +1,42 @@
+#    Copyright (c) 2026 Rich Bell <bellrichm@gmail.com>
+#
+#    See the file LICENSE.txt for your full rights.
+#
+
+# pylint: disable=wrong-import-order
+# pylint: disable=missing-module-docstring, missing-class-docstring, missing-function-docstring
+# pylint: disable=invalid-name
+
+# pylint: disable=line-too-long
+# pylint: disable=too-many-lines
+
+result_page_has_no_sections = \
+'''
+
+
+    <div class="container">
+
+
+    </div>
+      <script src="../charts/foo.js"></script>
+    
+    <!-- Modal -->
+<div class="modal" id="chartModal" tabindex="-1" role="dialog" aria-labelledby="chartModalTitle" aria-hidden="true">
+  <div class="modal-dialog modal-xl" role="document">
+    <div class="modal-content">
+      <div class="modal-header" id="chartModalHeader">
+        <h5 class="modal-title" id="chartModalTitle">Modal title</h5>
+        <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body" id="chartModalBody">
+        ...
+      </div>
+      <div class="modal-footer" id="chartModalFooter"></div>
+    </div>
+  </div>
+</div>
+
+
+'''
---
+++ bin/user/tests/unit/test_templates.py
@@ -0,0 +1,63 @@
+#    Copyright (c) 2026 Rich Bell <bellrichm@gmail.com>
+#
+#    See the file LICENSE.txt for your full rights.
+#
+
+# pylint: disable=wrong-import-order
+# pylint: disable=missing-module-docstring, missing-class-docstring, missing-function-docstring
+# pylint: disable=invalid-name
+
+import unittest
+
+import Cheetah.Template
+import os
+import types
+
+from user.tests import helpers
+
+from user.tests.unit.data.template_results.body_inc import result_page_has_no_sections
+
+class TestBodyInc(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        skin_dir = os.path.dirname(__file__) + '/../../../../skins/jas/'
+        os.chdir(skin_dir)
+
+    def test_page_has_no_sections(self):
+        self.maxDiff = None
+
+        page_name = 'foo4'
+        extras = types.SimpleNamespace(
+            pages = {
+                page_name: {
+                    'section': 'foo'
+                }
+            },
+            chart_definitions = 'foo',
+            current = {
+                'observations': {
+                    'obs1': {
+                        'display': False
+                    }
+                }
+            },
+        )
+
+        data = {
+            'page': page_name,
+            'Extras': extras,
+            'page_name_global': 'foo',
+        }
+
+        filename = 'generators/body.inc'
+
+        template_class = Cheetah.Template.Template.compile(file=filename)
+        # print(f"----\n{Cheetah.Template.Template.generatedModuleCode(template_class)}\n----")
+        template_instance = template_class(searchList=[data])
+        result = template_instance.respond()
+        self.assertEqual(result, result_page_has_no_sections)
+
+        print("done")
+
+if __name__ == '__main__':
+    helpers.run_tests()
